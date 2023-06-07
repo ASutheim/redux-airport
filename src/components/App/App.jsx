c02374a8386b1385ad5ca05f36e628ea06b32d79
Anna Sutheim <asutheim@gmail.com>
2023-06-07 07:01:02 -0500

    App js file set up to submit data on button click and return airline list component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import AirlineList from '../AirlineList/AirlineList'
 
 function App() {
 
+  const reduxStore = useSelector(store => store);
+  const dispatch = useDispatch();
+  
   return (
     <div>
       <h1>Redux Airport</h1>
       <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <pre>{JSON.stringify(reduxStore)}</pre>
+      <button onClick={() => dispatch({ type: 'ADD_AIRLINE', payload: 'airline' })}>Add Airline</button>
+      <table>
+        <AirlineList/>
+        </table>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
 
 /** TODO: import REDUX **/
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
 
 /** TODO: Add REDUCERS */
 
+const airlineList = (state = [], action) => {
+    if (action.type === 'ADD_AIRLINE')
+    // 
+}
 
 /** TODO: Create store */
 
-
+const storeInstance = createStore(
+    combineReducers({
+     //reducer names
+    }),
+    applyMiddleware(logger)
+  );
+  
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider>
+    <App />
+    </Provider>
+  </React.StrictMode>
 );
